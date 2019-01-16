b3e988bea8d48a2bb626f3b46ac44fa9d513ea83
quangthang.le <quangthang.le@tanchonggroup.com>
2019-01-16 17:48:44 +0700

    switch to dio

--- lib/src/request_loader.dart
+++ lib/src/request_loader.dart
@@ -1,3 +1,4 @@
+import 'dart:_http';
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_base_request/src/constant.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_base_request/src/request_callback.dart';
 import 'package:flutter_base_request/src/request_exception.dart';
 import 'package:flutter_base_request/src/request_type.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 class BaseRequestLoader<T> {
   String _baseUrl;
@@ -69,53 +71,52 @@ class BaseRequestLoader<T> {
   }
 
   Future request() async {
-    if (_endPointUrl.isEmpty) throw Exception('URL is empty!!');
-    http.Response response;
+    if (_endPointUrl.isEmpty || _baseUrl.isEmpty) throw Exception('URL is empty!!');
+
     if (_callback != null) {
       _callback.onStart();
     }
     try {
-      String url = _baseUrl + _endPointUrl;
       if (_newBaseUrl != null && _newBaseUrl.length > 0) {
-        url = _newBaseUrl + _endPointUrl;
+        _baseUrl = _newBaseUrl;
       }
-      _headers["content-type"] = "application/json";
+      _headers[HttpHeaders.contentTypeHeader] = "application/json";
       if (_isAuthRequest && _authToken.length > 0) {
-        _headers["authorization"] = "bearer " + _authToken;
+        _headers[HttpHeaders.authorizationHeader] = "bearer $_authToken";
       }
+      // set options
+      Dio dio = Dio();
+      dio.options.baseUrl = _baseUrl;
+      dio.options.headers = _headers;
+      dio.options.connectTimeout = _timeout;
+      dio.options.receiveTimeout = _timeout;
+
       print(
-          "RequestLoader=>url= $url \n params= $_params \n isAuthor=$_isAuthRequest \n headers= _$_headers");
+          "RequestLoader=>baseurl= $_baseUrl \n endPointUrl= $_endPointUrl \n params= $_params \n isAuthor=$_isAuthRequest \n headers= _$_headers");
+
+      Response response;
       switch (_requestType) {
         case BaseRequestType.POST:
-          response = await http
-              .post(url, headers: _headers, body: json.encode(_params))
-              .timeout(Duration(seconds: _timeout));
+          response = await dio.post(_endPointUrl, data: json.encode(_params));
           break;
         case BaseRequestType.GET:
-          response = await http
-              .get(url, headers: _headers)
-              .timeout(Duration(seconds: _timeout));
+          response = await dio.get(_endPointUrl, data: json.encode(_params));
           break;
         case BaseRequestType.PUT:
-          response = await http
-              .put(url, headers: _headers, body: json.encode(_params))
-              .timeout(Duration(seconds: _timeout));
+          response = await dio.put(_endPointUrl, data: json.encode(_params));
           break;
         default:
-          response = await http
-              .get(url, headers: _headers)
-              .timeout(Duration(seconds: _timeout));
+          response = await dio.get(_endPointUrl, data: json.encode(_params));
       }
 
       if (response != null) {
         int statusCode = response.statusCode;
-        String jsonResponse = response.body;
-        print(
-            "RequestLoader=>url= $url \n statusCode= $statusCode \n json=$jsonResponse");
+        String jsonResponse = response.data;
+        print("RequestLoader=>statusCode= $statusCode \n json=$jsonResponse");
         if (_callback != null) {
           if (statusCode < BaseConstant.statusCodeSuccess ||
               statusCode >= BaseConstant.statusCodeError) {
-            _callback.onError(new BaseRequestException(statusCode, response.body));
+            _callback.onError(new BaseRequestException(statusCode, jsonResponse));
           } else {
             _callback.onCompleted(json.decode(jsonResponse));
           }
@@ -126,4 +127,64 @@ class BaseRequestLoader<T> {
       if (_callback != null) _callback.onError(e);
     }
   }
+
+// request with http lib
+//  Future httpRequest() async {
+//    if (_endPointUrl.isEmpty) throw Exception('URL is empty!!');
+//    http.Response response;
+//    if (_callback != null) {
+//      _callback.onStart();
+//    }
+//    try {
+//      String url = _baseUrl + _endPointUrl;
+//      if (_newBaseUrl != null && _newBaseUrl.length > 0) {
+//        url = _newBaseUrl + _endPointUrl;
+//      }
+//      _headers["content-type"] = "application/json";
+//      if (_isAuthRequest && _authToken.length > 0) {
+//        _headers["authorization"] = "bearer " + _authToken;
+//      }
+//      print(
+//          "RequestLoader=>url= $url \n params= $_params \n isAuthor=$_isAuthRequest \n headers= _$_headers");
+//      switch (_requestType) {
+//        case BaseRequestType.POST:
+//          response = await http
+//              .post(url, headers: _headers, body: json.encode(_params))
+//              .timeout(Duration(seconds: _timeout));
+//          break;
+//        case BaseRequestType.GET:
+//          response = await http
+//              .get(url, headers: _headers)
+//              .timeout(Duration(seconds: _timeout));
+//          break;
+//        case BaseRequestType.PUT:
+//          response = await http
+//              .put(url, headers: _headers, body: json.encode(_params))
+//              .timeout(Duration(seconds: _timeout));
+//          break;
+//        default:
+//          response = await http
+//              .get(url, headers: _headers)
+//              .timeout(Duration(seconds: _timeout));
+//      }
+//
+//      if (response != null) {
+//        int statusCode = response.statusCode;
+//        String jsonResponse = response.body;
+//        print(
+//            "RequestLoader=>url= $url \n statusCode= $statusCode \n json=$jsonResponse");
+//        if (_callback != null) {
+//          if (statusCode < BaseConstant.statusCodeSuccess ||
+//              statusCode >= BaseConstant.statusCodeError) {
+//            _callback.onError(new BaseRequestException(statusCode, response.body));
+//          } else {
+//            _callback.onCompleted(json.decode(jsonResponse));
+//          }
+//        }
+//      }
+//    } catch (e) {
+//      print("error=" + e.toString());
+//      if (_callback != null) _callback.onError(e);
+//    }
+//  }
 }
