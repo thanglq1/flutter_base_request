2f7fd392877c02f3be59c09fdcd00b218b9a515b
quangthang.le <quangthang.le@tanchonggroup.com>
2019-01-17 10:24:29 +0700

    export

--- lib/flutter_base_request.dart
+++ lib/flutter_base_request.dart
@@ -3,4 +3,5 @@ library flutter_base_request;
 export 'src/request_factory.dart';
 export 'src/request_callback.dart';
 export 'src/request_method.dart';
+export 'src/request_type.dart';
 export 'src/request_exception.dart';
